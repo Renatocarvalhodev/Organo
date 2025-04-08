1f79047ff37af8758c2791b113a280dfa4ac34ab
Renato Carvalho <renato.itdevcarvalho@gmail.com>
2025-04-08 12:34:08 -0300

    Componetes CampoTexto e Formulario criados e estilizados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componets/Banner/Banner';
+import Formulario from './componets/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario />
     </div>
   );
 }
