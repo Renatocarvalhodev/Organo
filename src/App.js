88211c521cb3566d651dfa177c87f0cb41f44aac
Renato Carvalho <renato.itdevcarvalho@gmail.com>
2025-04-10 12:30:20 -0300

    Captura dos dados inseridos no formulário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import Banner from './componets/Banner/Banner';
 import Formulario from './componets/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState('')
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  } 
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
